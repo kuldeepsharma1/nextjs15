abbfbafef87728371afe0962fe81236505f04131
kuldeepsharma1 <hytek112@gmail.com>
2024-11-20 23:31:42 +0530

    Auth v0 done

--- ipost/src/app/auth/login/page.tsx
+++ ipost/src/app/auth/login/page.tsx
@@ -97,9 +97,9 @@ export default function Login() {
                 Password
               </label>
               <div className="text-sm">
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                <Link href="/auth/forgot" className="font-semibold text-indigo-600 hover:text-indigo-500">
                   Forgot password?
-                </a>
+                </Link>
               </div>
             </div>
             <div className="mt-2">
