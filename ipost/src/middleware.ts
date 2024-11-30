d236ba8ab9fa78292eb6f73a335904bfeca705f9
kuldeepsharma1 <hytek112@gmail.com>
2024-12-01 01:01:02 +0530

    small

--- ipost/src/middleware.ts
+++ ipost/src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest){
     const path = request.nextUrl.pathname
-    const isPublicPath = path === '/auth/login' || path === '/auth/register'|| path === '/auth/register'
+    const isPublicPath = path === '/auth/login' || path === '/auth/register'|| path === '/auth/verifyemail'
 
     const token = request.cookies.get('token')?.value || ''
     if(isPublicPath && token){
--- ipost/src/models/userModel.ts
+++ ipost/src/models/userModel.ts
@@ -1,37 +1,37 @@
-import mongoose,{Schema} from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 
 
 const userSchema = new Schema(
     {
-        username:{
-            type:String,
-            required:[true,"Please give me username"],
-            unique:true,
+        username: {
+            type: String,
+            required: [true, "Please give me username"],
+            unique: true,
         },
-        email:{
-            type:String,
-            required:[true,"Please give me email"],
-            unique:true,
+        email: {
+            type: String,
+            required: [true, "Please give me email"],
+            unique: true,
         },
-        password:{
-            type:String,
-            required:[true,"Please give me password"],
+        password: {
+            type: String,
+            required: [true, "Please give me password"],
         },
-        isVerified:{
-            type:Boolean,
-            default:false,
+        isVerified: {
+            type: Boolean,
+            default: false,
         },
-        isAdmin:{
-            type:Boolean,
-            default:false,
+        isAdmin: {
+            type: Boolean,
+            default: false,
         },
-forgotPasswordToken:String,
-forgotPasswordTokenExpiry:Date,
-verifyToken:String,
-verifyTokenExpiry:Date,
-})
+        forgotPasswordToken: String,
+        forgotPasswordTokenExpiry: Date,
+        verifyToken: String,
+        verifyTokenExpiry: Date,
+    })
 
-const User = mongoose.models.users || mongoose.model("users",userSchema);
+const User = mongoose.models.users || mongoose.model("users", userSchema);
 
 export default User;
 
