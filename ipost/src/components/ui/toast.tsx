64857d8276ef4b27d269fd528d37775740e4b3e8
kuldeepsharma1 <hytek112@gmail.com>
2024-12-08 12:03:34 +0530

    stabe

--- ipost/src/components/ui/toast.tsx
+++ ipost/src/components/ui/toast.tsx
@@ -32,8 +32,6 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
-          success:
-          "border bg-background text-green-600",
       },
     },
     defaultVariants: {
